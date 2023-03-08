181b602df07092282ad407e1c9da274bb8e21b5f
aamindehkordi <ali.amindehkordi@eagles.cui.edu>
2023-03-08 12:52:00 -0800

    cleanup?

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import tools.cleanup as cleanup, tools.process as process, tools.transcribe as transcribe, gui.gui as gui
+import tools.cleanup as cleanup, tools.process as process, tools.transcribe_local as transcribe_local, gui.gui as gui
 import json, tkinter as tk
 
 def transcribe_audio(model_size, folder):
     for i in range(0, 6):
         filename = f"./data/videos/{folder}/{i}.m4a"
         print(f"~~~~~~~~~~\n Transcribing {filename}...\n")
-        result = transcribe.transcribe(filename, model_size)
+        result = transcribe_local.transcribe(filename, model_size)
         process.write_to_file(f"./data/processed/{folder}/{i}.json", json.dumps(result))
         print(f"Transcription result: {json.dumps(result['text'])[:100]} \n~~~~~~~~~~ \nEND\n~~~~~~~~~~~")
 
@@ -14,7 +14,7 @@ def main():
     Main function.
     """
     # Transcribe audio from 1-5.mp3
-    transcribe_audio("medium", "kyle")
+    #transcribe_audio("medium", "kyle")
     # Extract messages
     #messages = cleanup.save_messages_csv(cleanup.extract_messages_csv("./data/preprocessed/goobahahhgoobah.csv"))
     # Print messages

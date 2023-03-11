7955199afbd875c67ec445c32f05c395c207dd58
aamindehkordi <ali.amindehkordi@eagles.cui.edu>
2023-03-10 16:49:24 -0800

    updated randomness and still nothing

--- api/chat.py
+++ api/chat.py
@@ -35,17 +35,18 @@ def get_response_nathan(input_msg, history):
   {'role':'user', 'content':'I just ate an oyster'},
   {'role':'assistant', 'content':'Who eats oysters theyre like the little cum dumpsters of the sea'},
   #{'role':'user', 'content':'Ok let\'s move on'},
-  *history]
+  *history
+  ]
   print(history)
   response = openai.ChatCompletion.create(
   model="gpt-3.5-turbo", # the name of the model to use
   messages=msgs,
-  temperature=0.333, #What sampling temperature to use, between 0 and 2. Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic. We generally recommend altering this or top_p but not both.
+  temperature=1, #What sampling temperature to use, between 0 and 2. Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic. We generally recommend altering this or top_p but not both.
   top_p=1, #An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered.
-  n=1, #How many chat completion choices to generate for each input_msg message.
+  n=3, #How many chat completion choices to generate for each input_msg message.
   stream=False, #If set, partial message deltas will be sent, like in ChatGPT. Tokens will be sent as data-only server-sent events as they become available, with the stream terminated by a data: [DONE] message.
   stop= "null",
-  max_tokens=2000,
+  max_tokens=200,
   presence_penalty=0,  
   frequency_penalty=0,
   logit_bias={})
@@ -100,12 +101,12 @@ def get_response_ali(input_msg, history):
   response = openai.ChatCompletion.create(
   model="gpt-3.5-turbo", # the name of the model to use
   messages=msgs,
-  temperature=0.333, #What sampling temperature to use, between 0 and 2. Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic. We generally recommend altering this or top_p but not both.
+  temperature=1, #What sampling temperature to use, between 0 and 2. Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic. We generally recommend altering this or top_p but not both.
   top_p=1, #An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered.
-  n=1, #How many chat completion choices to generate for each input_msg message.
+  n=2, #How many chat completion choices to generate for each input_msg message.
   stream=False, #If set, partial message deltas will be sent, like in ChatGPT. Tokens will be sent as data-only server-sent events as they become available, with the stream terminated by a data: [DONE] message.
   stop= "null",
-  max_tokens=2000,
+  max_tokens=150,
   presence_penalty=0,  
   frequency_penalty=0,
   logit_bias={})

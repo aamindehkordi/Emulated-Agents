138edd3c573b6c85607f193a4851a95d50100571
aamindehkordi <ali.amindehkordi@eagles.cui.edu>
2023-03-14 21:51:48 -0700

    abstraction

--- controller/chat_controller.py
+++ controller/chat_controller.py
@@ -1,6 +1,14 @@
 from .base_controller import BaseController
-import model.chat as chat
-
+from model.agents.ali.ali import get_response_ali
+from model.agents.jett.jett import get_response_jett
+from model.agents.nathan.nathan import get_response_nathan
+from model.agents.kate.kate import get_response_kate
+from model.agents.robby.robby import get_response_robby
+from model.agents.cat.cat import get_response_cat
+from model.openai_api import get_response
+"""
+Handles all the openai API stuff and user responses.
+"""
 class ChatController(BaseController):
     def __init__(self, gui):
         super().__init__(gui)
@@ -20,16 +28,16 @@ class ChatController(BaseController):
 
     def get_bot_response(self, bot, chat_history):
         if bot == "All":
-            response, chat_history = chat.get_response_all(chat_history)
+            response, chat_history = self.get_response_all(chat_history)
         else:
             bot_response_function = {
-                "Ali": chat.get_response_ali,
-                "Nathan": chat.get_response_nathan,
-                #"Kyle": chat.get_response_kyle,
-                "Robby": chat.get_response_robby,
-                "Jett": chat.get_response_jett,
-                "Kate": chat.get_response_kate,
-                "Cat": chat.get_response_cat,
+                "Ali": get_response_ali,
+                "Nathan":get_response_nathan,
+                #"Kyle":get_response_kyle,
+                "Robby":get_response_robby,
+                "Jett":get_response_jett,
+                "Kate":get_response_kate,
+                "Cat": get_response_cat,
                 #"Jake": chat.get_response_jake,
             }
             response = bot_response_function[bot](chat_history)
@@ -53,3 +61,44 @@ class ChatController(BaseController):
         Handles the target user dropdown change event in the chat GUI.
         """
         pass
+    
+    def get_response_todo(self, user, message, target_user):
+        """
+        Given the user, message, and target user, returns the AI-generated response.
+        
+        Args:
+            user (str): The user sending the message.
+            message (str): The content of the message.
+            target_user (str): The user expected to respond.
+        
+        Returns:
+            response (str): The AI-generated response.
+        """
+        pass
+
+    def get_response_all(self,history):
+        """
+            Get Responses from all agents and formats them into a chat history list
+            
+            *args:
+            history: list of chat history
+            
+            *returns:
+            updated history in this format {'role':'user', 'content':f"{user}: {message}"}
+        """
+        user_list = ['nathan', 'ali', 'jett', 'kate', 'robby', 'cat'] #add more users here
+        responses = []
+        #Get responses from all agents
+        for user in user_list:
+            response = get_response(user, history)
+            history.append({'role':'assistant', 'content':f"{user}: {response}"})
+            
+        #Update history
+        history = [*history, *responses]
+        
+        # Clean up responses for display
+        for response in responses:
+            response['content'] = response['content'].replace('{user}:', '')
+        
+        return responses, history
+    
---
+++ model/openai_api.py
@@ -0,0 +1,47 @@
+import openai
+
+def get_response(user, history):
+  """
+    Gets appropriate user chat response based off the chat history.
+    
+    *args:
+    user: a string of the user's name
+    history: chat history from this session
+  
+    *returns:
+    response: a string containing the chat response
+  """
+  #Read Agent Prompt from file
+  with open(f"model/agents/{user}/{user}_prompt.txt", encoding='utf-8') as f:
+    agentPrompt = f.read()
+  
+  #Read general knowledge from file
+  with open("model/agents/general_knowledge.txt", encoding='utf-8') as f:
+    general = f.read()
+    
+  msgs = [
+    {'role':'user', 'content': f'{agentPrompt}\n{general}'},
+    *history
+  ]
+  
+  response = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo-0301", # the name of the model to use
+  messages=msgs,
+  temperature=1, #What sampling temperature to use, between 0 and 2. Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic. We generally recommend altering this or top_p but not both.
+  top_p=1, #An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered.
+  n=2, #How many chat completion choices to generate for each input_msg message.
+  stream=False, #If set, partial message deltas will be sent, like in ChatGPT. Tokens will be sent as data-only server-sent events as they become available, with the stream terminated by a data: [DONE] message.
+  stop= "null",
+  max_tokens=200,
+  presence_penalty=0,  
+  frequency_penalty=0,
+  )
+  
+  answer = response["choices"][1]["message"]["content"] # type: ignore
+  
+  #Failsafe
+  """
+  if "language model" in answer or "OpenAI" in answer or "I was created" in answer:
+    answer = "Really bro. That's what you want to talk about? Talk about something else."
+  """
+  return  answer

65fcd5085282d9ca8f4332519fcd5e8eb87e0568
aamindehkordi <ali.amindehkordi@eagles.cui.edu>
2023-03-18 13:32:17 -0700

    abstracted parameters to function

--- model/openai_api.py
+++ model/openai_api.py
@@ -34,13 +34,22 @@ transcript = transcribe_video("/Users/ali/Library/CloudStorage/OneDrive-Personal
 print(transcript)
 """
 
-def get_response(user, history):
+def get_response(user, history, model="gpt-3.5-turbo", temperature=0.91, top_p=1, n=3, stream=False, stop= "null", max_tokens=350, presence_penalty=0, frequency_penalty=0):
   """
     Gets appropriate user chat response based off the chat history.
     
     *args:
     user: a string of the user's name
     history: chat history from this session
+    model: the name of the model to use
+    temperature: What sampling temperature to use, between 0 and 2. Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic. We generally recommend altering this or top_p but not both.
+    top_p: An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered.
+    n: How many chat completion choices to generate for each input_msg message.
+    stream: If set, partial message deltas will be sent, like in ChatGPT. Tokens will be sent as data-only server-sent events as they become available, with the stream terminated by a data: [DONE] message.
+    stop: Up to 4 sequences where the API will stop generating further tokens.
+    max_tokens: The maximum number of tokens to generate.
+    presence_penalty: Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+    frequency_penalty: Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
   
     *returns:
     response: a string containing the chat response
@@ -60,16 +69,16 @@ def get_response(user, history):
   ]
   
   response = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo", # the name of the model to use
+  model=model, # the name of the model to use
   messages=msgs,
-  temperature=0.91, #What sampling temperature to use, between 0 and 2. Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic. We generally recommend altering this or top_p but not both.
-  top_p=1, #An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered.
-  n=3, #How many chat completion choices to generate for each input_msg message.
-  stream=False, #If set, partial message deltas will be sent, like in ChatGPT. Tokens will be sent as data-only server-sent events as they become available, with the stream terminated by a data: [DONE] message.
-  stop= "null",
-  max_tokens=500,
-  presence_penalty=0,  
-  frequency_penalty=0,
+  temperature=temperature, #What sampling temperature to use, between 0 and 2. Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic. We generally recommend altering this or top_p but not both.
+  top_p=top_p, #An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered.
+  n=n, #How many chat completion choices to generate for each input_msg message.
+  stream=stream, #If set, partial message deltas will be sent, like in ChatGPT. Tokens will be sent as data-only server-sent events as they become available, with the stream terminated by a data: [DONE] message.
+  stop= stop,
+  max_tokens=max_tokens,
+  presence_penalty=presence_penalty,  
+  frequency_penalty=frequency_penalty,
   )
   
   answer = response["choices"][1]["message"]["content"] # type: ignore

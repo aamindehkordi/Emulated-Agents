50d15cdcaf8e7e700bd85f23d6f7a134820b928e
aamindehkordi <ali.amindehkordi@eagles.cui.edu>
2023-03-22 00:00:35 -0700

    now it works

--- model/openai_api.py
+++ model/openai_api.py
@@ -122,7 +122,7 @@ def get_response(user, history, model="gpt-3.5-turbo", temperature=0.91, top_p=1
     except Exception as oops:
         if 'maximum context length' in str(oops):
             #TODO better solution
-            msgs = msgs[len(msgs)//2-1:len(msgs)//2+1]
+            msgs = msgs[1:]
             continue
         retry += 1
         if retry >= max_retry:

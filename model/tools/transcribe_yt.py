4ca11297186b1065ca7af0bcb97fdbe30d110916
aamindehkordi <ali.amindehkordi@eagles.cui.edu>
2023-03-23 15:59:13 -0700

    updates

--- model/tools/transcribe_yt.py
+++ model/tools/transcribe_yt.py
@@ -7,31 +7,36 @@ class YoutubeTranscript():
     def download_audio_from_youtube(self,url):
         yt = YouTube(url)
         stream = yt.streams.filter(only_audio=True).first()
-        fn = "temp.wav"
+        fn = yt.title + ".wav"
         stream.download(filename=fn, output_path=".", skip_existing=False)
         return fn
 
-    def transcribe_audio(self,file_path):
+    def transcribe_audio(self,file_path, write=False):
         model = whisper.load_model("medium")
         audio = whisper.load_audio(file_path)
         result = model.transcribe(audio)
+        
+        if write:
+            #Save the transcript to a file with the same name as the audio file
+            fn = file_path.split(".")[0] + ".txt"
+            with open(fn, "w") as f:
+                f.write(result["text"])
         return result["text"]
-
+    
 if __name__ == "__main__":
     lx = YoutubeTranscript()
     if len(sys.argv) < 2:
-        print("Usage: python youtube_transcript.py <youtube_url>")
+        print("Usage: python youtube_transcript.py <youtube_url or file_path>")
         sys.exit(1)
 
     youtube_url = sys.argv[1]  
     try:                          
         audio_file = lx.download_audio_from_youtube(youtube_url)
-        transcript = lx.transcribe_audio(audio_file)
-        os.remove(audio_file)   
+        transcript = lx.transcribe_audio(audio_file, True)
     except Exception as e:
         print(e)
         try:
-            transcript = lx.transcribe_audio(youtube_url)
+            transcript = lx.transcribe_audio(youtube_url, True)
         except Exception as el:
             print("L bozo")
             transcript = "Error: Could not transcribe audio."

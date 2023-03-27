8c94d8a992eaf79d52c48a31c5034c06ad8c2b10
aamindehkordi <ali.amindehkordi@eagles.cui.edu>
2023-03-27 15:34:41 -0700

    more complete base controller

--- controller/base_controller.py
+++ controller/base_controller.py
@@ -1,28 +1,53 @@
 """
+./controller/base_controller.py
 This module provides the BaseController class, a base class for various controllers in the application.
 
 Classes in this module include:
 - BaseController: The base class for other controllers, with common functionality.
 """
+import tkinter as tk
+from view.base_gui import BaseGUI
+from view.discord_gui import DiscordGUI
+from model.base_model import BaseModel
+# Import the ZoomGUI and PhotoboothGUI when they are implemented
+# from view.zoom_gui import ZoomGUI
+# from view.photobooth_gui import PhotoboothGUI
 
 class BaseController:
-    def __init__(self, gui, model):
-        self.gui = gui
-        self.model = model
-        self.token_count = 0
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.withdraw()  # Hide the root window
+        self.model = BaseModel()
+        self.base_gui = BaseGUI(self)
+        self.chat_gui = DiscordGUI(self)
+        self.chat_gui.withdraw()  # Hide the chat_gui initially
 
-    def update_view(self):
-        pass
+        # Uncomment these lines when the ZoomGUI and PhotoboothGUI are implemented
+        # self.zoom_gui = ZoomGUI(self)
+        # self.zoom_gui.withdraw()
+        #
+        # self.photobooth_gui = PhotoboothGUI(self)
+        # self.photobooth_gui.withdraw()
+
+    def run(self):
+        self.root.mainloop()
+
+    def switch_to_chat_mode(self):
+        self.base_gui.withdraw()
+        self.chat_gui.deiconify()
+        
+    def switch_to_base_mode(self):
+        self.chat_gui.withdraw()
+        self.base_gui.deiconify()
+        
+    def switch_to_zoom_mode(self):
+        self.base_gui.withdraw()
+        #self.zoom_gui.deiconify()
+        
+    def switch_to_photobooth_mode(self):
+        self.base_gui.withdraw()
+        #self.photobooth_gui.deiconify()
     
-    def on_mode_change(self, mode):
-        """
-        Handles the mode change event.
-
-        Args:
-            mode (str): The new mode (e.g., 'chat', 'zoom', 'photobooth').
-        """
-        pass
-
     def on_exit(self):
         """
         Handles the exit event for the application.

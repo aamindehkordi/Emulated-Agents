755ec2da2af44354354607e750762d0abed568c0
nathanmaldonado <nathan.maldonado@eagles.cui.edu>
2023-03-27 20:14:22 -0700

    JSON history files can now be added to and formatted correctly

--- controller/chat_controller.py
+++ controller/chat_controller.py
@@ -2,6 +2,8 @@
 from .base_controller import BaseController
 from model.tools.code_extractor import FileSearcher
 import ast
+import json
+import os
 
 class ChatController(BaseController):
     def __init__(self, model):
@@ -50,7 +52,9 @@ class ChatController(BaseController):
         self.token_count = self.token_count + tokens[0]
         chat_history.append({'role': 'assistant', 'content': f"{response}"})
 
-        return response    
+        self.append_response_to_json_file(message =response, is_assistant= 1, file_path = './model/history/nathan_history.json')
+
+        return response
 
     
     def get_response_developer(self, agent, history):
@@ -102,9 +106,41 @@ class ChatController(BaseController):
             classes = [node.name for node in ast.walk(parsed_ast) if isinstance(node, ast.ClassDef)]  # Extract class names from the AST
             all_classes.extend(classes)
         return all_classes
-    
-        
-    
+
+    def append_response_to_json_file(self, message, is_assistant, file_path):
+        is_assistant = bool(is_assistant)
+        if is_assistant == 0:
+            entry = {
+                "role": "user",
+                "content": message
+            }
+        else:
+            entry = {
+                "role": "assistant",
+                "content": message
+            }
+
+        # Check if the file exists and create it if it doesn't
+        if not os.path.exists(file_path):
+            with open(file_path, 'w') as f:
+                f.write('[\n')
+
+        # Read the existing content of the file
+        with open(file_path, 'r') as f:
+            data = f.readlines()
+
+        # If there is already content in the file, add a comma after the last JSON object
+        if len(data) > 1:
+            file_size = os.path.getsize(file_path)
+            with open(file_path, 'r+') as f:
+                f.seek(file_size - 2)
+                f.write(',\n')
+
+        # Append the entry as a JSON string followed by a newline character
+        with open(file_path, 'a') as f:
+            json_entry = json.dumps(entry, indent=2)
+            f.write(json_entry + "\n]")
+
     #TODO
     def get_response_all(self,history):
         """
@@ -137,5 +173,4 @@ class ChatController(BaseController):
         #called from the gui
         #self.model.save_history()
         self.on_exit()
-        
-    
+

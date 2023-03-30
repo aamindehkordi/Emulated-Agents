3795d5699c39eaa66644e2e4e58a6743de02aa98
aamindehkordi <ali.amindehkordi@eagles.cui.edu>
2023-03-29 23:14:06 -0700

    updated base_model.py

--- model/base_model.py
+++ model/base_model.py
@@ -7,7 +7,6 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Pinecone
 from langchain.document_loaders import TextLoader
-from langchain.document_loaders import TextLoader
 from langchain.llms import OpenAI
 import pinecone
 
@@ -16,6 +15,22 @@ class BaseModel:
     def __init__(self):
         self.agents = {}
         self.initialize_agents()
+        self.key = []
+        with open('./key_openai.txt') as f:
+            self.key.append(f.readline().strip())
+
+        # Read the API key and environment from file lines 1 and 2
+        with open('./key_pinecone.txt') as f:
+            self.key.append(f.readline().strip())
+            self.environment = f.readline().strip()
+
+        # initialize pinecone
+        pinecone.init(
+            api_key=self.key[1],  # find at app.pinecone.io
+            environment=self.environment  # next to api key in console
+        )
+
+        self.pine_index = "ai-langchain"
 
     def initialize_agents(self):
         agent_names = ['ali', 'nathan', 'jett', 'kate', 'robby', 'cat', 'kyle', 'jake', 'developer']
@@ -36,7 +51,6 @@ class BaseModel:
             agent: agent object
             history: list of chat history
             debug: boolean for debug mode (developer)
-            
         """
         if str(agent.name) == "developer":
             debug = True
@@ -139,53 +153,38 @@ class BaseModel:
                 print(f'Error communicating with OpenAI: {oops}. Retrying in {2 ** (retry - 1) * 5} seconds...')
                 sleep(2 ** (retry - 1) * 5)
 
-    def generate_response(self, query, agent_history, agent_prompt):
+    def generate_response(self, query, agent_msgs, agent_prompt):
         """
         """
         loader = TextLoader('model/prompts/super_prompt.txt')
         documents = loader.load()
 
-        text_splitter = CharacterTextSplitter(chunk_size=300, chunk_overlap=100)
+        text_splitter = CharacterTextSplitter(chunk_size=200, chunk_overlap=50)
         docs = text_splitter.split_documents(documents)
-        
 
-        with open('./key_openai.txt') as f:
-            key = f.readline().strip()
-        embeddings = OpenAIEmbeddings(openai_api_key=key) # type: ignore
 
-        # Read the API key and environment from file lines 1 and 2
-        with open('./key_pinecone.txt') as f:
-            api_key = f.readline().strip()
-            environment = f.readline().strip()
-        # initialize pinecone
-        pinecone.init(
-            api_key=api_key,  # find at app.pinecone.io
-            environment=environment  # next to api key in console
-        )
-        
-        index_name = "ai-langchain"
+        embeddings = OpenAIEmbeddings(openai_api_key=self.key[0])  # type: ignore
 
-        docsearch = Pinecone.from_documents(docs, embeddings, index_name=index_name)
+        docsearch = Pinecone.from_documents(docs, embeddings, index_name=self.pine_index)
         docs = docsearch.similarity_search(query)
-
         relevant_doc = docs[0].page_content
-        print(f'relevant doc:\n{relevant_doc}')
-        msg = [{'role':'system', 'content': f'{agent_prompt}'}, 
-            *agent_history, 
-            { "role": "user", "content": 
-                f"""
-                Maintain your persona and respond appropriately to this message [{query}]
+        msg = [
+                {"role": "user", "content": f"""
+                Maintain your persona and respond appropriately in the following discord conversation.
                 To assist you in the conversation, you may be provided with some information. 
                 Only use information from the following information, if the user refers to it.
                 Do not use multiple lines of information from the document.
-                Information:\n\n{relevant_doc}"""}]
+                Information:\n\n{relevant_doc}\n\n Discord Conversation Hisotry:\n"""},
+                *agent_msgs,
+                {"role": "user", "content": f"{query}"}
+               ]
         
         response = openai.ChatCompletion.create(messages=msg, model="gpt-3.5-turbo", temperature=0.91, top_p=1, n=1, stream=False, stop= "null", max_tokens=350, presence_penalty=0, frequency_penalty=0)
         answer = response["choices"][0]["message"]["content"] # type: ignore
 
         tokens = (response['usage']['total_tokens'],) # type: ignore
 
-        print(answer, tokens)
+        print(msg, answer, tokens)
         return answer, tokens
     
     def transcribe_video(self, fn_in, model="medium", prompt="", language="en", fp16=False, temperature=0):

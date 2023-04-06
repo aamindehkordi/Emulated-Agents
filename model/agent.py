edfd8349ee3715c5f88972c647546929e32212f6
aamindehkordi <ali.amindehkordi@eagles.cui.edu>
2023-04-06 02:28:20 -0700

    With new improvements, only 0.005/message with gpt-4

--- model/agent.py
+++ model/agent.py
@@ -5,21 +5,24 @@ class Agent:
     def __init__(self, name, prompt_path, history_path):
         self.name = name
         self.prompt_path = prompt_path
+        self.general_path = "./model/prompts/general_knowledge.txt"
         self.history_path = history_path
         self.msgs = []
-        self.model = "gpt-3.5-turbo"
+        self.model = "gpt-4"
         self.max_tokens = 350
 
     def get_prompt(self):
         try:
+            with open(self.general_path, 'r', encoding='utf-8') as f:
+                self.general = f.read()
             with open(self.prompt_path, 'r', encoding='utf-8') as f:
                 self.prompt = f.read()
-                return self.prompt
+                return self.prompt, self.general
         except Exception as e:
             print(e)
             return ''
 
-    def get_history(self):
+    def get_priming(self):
         try:
             with open(self.history_path, 'r', encoding='utf-8') as f:
                 self.history = json.load(f)

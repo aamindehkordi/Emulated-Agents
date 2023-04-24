e28a9547a527ad4a26f7a7d9d48ff9a4d61ed235
aamindehkordi <ali.amindehkordi@eagles.cui.edu>
2023-04-24 01:59:14 -0700

    starting zoom implementation

--- view/zoom_gui.py
+++ view/zoom_gui.py
@@ -1,36 +1,35 @@
 """
 ./view/zoom_gui.py
 """
+import tkinter as tk
+from tkinter import ttk
+from view.base_gui import BaseGUI
 
 
-import pygame
-import view.base_gui as gui
-class ZoomGUI(gui.BaseGUI):
-    def __init__(self, master):
-        super().__init__(master)
-        # Create a grid layout for video feeds
+class ZoomGUI(BaseGUI):
+    def __init__(
+        self, controller, theme="equilux", font=("Arial", 12), padx=10, pady=10
+    ):
+        super().__init__(controller, theme, font, padx, pady)
+        self.geometry("1400x600")
+        self.title("AI Friends Zoom Mode")
 
+    def create_main_frame(self):
+        main_frame = ttk.Frame(self)
+        main_frame.pack(expand=True, fill=tk.BOTH, padx=self.padx, pady=self.pady)
 
-    def update_video_feed(self, user, video):
-        """
-        Updates the video feed for the specified user.
-        
-        Args:
-            user (str): The user whose video feed needs to be updated.
-            video (bytes): The input video as bytes.
-        """
-        pass
+        grid_frame = ttk.Frame(main_frame)
+        grid_frame.pack(expand=True, fill=tk.BOTH, padx=self.padx, pady=self.pady)
 
-    @staticmethod
-    def play_audio(user, audio):
-        """
-        Plays the audio for the specified user.
-        
-        Args:
-            user (str): The user whose audio needs to be played.
-            audio (bytes): The input audio as bytes.
-        """
-        # Play the audio for the specified user
-        pygame.mixer.init()
-        pygame.mixer.music.load(audio)
-        pygame.mixer.music.play()
+        self.create_video_grid(grid_frame)
+
+    def create_video_grid(self, grid_frame):
+        for i in range(2):
+            for j in range(3):
+                placeholder_label = ttk.Label(
+                    grid_frame,
+                    text=f"Agent Video {i * 3 + j + 1}",
+                    relief=tk.RAISED,
+                    width=20,
+                )
+                placeholder_label.grid(row=i, column=j, padx=self.padx, pady=self.pady)
